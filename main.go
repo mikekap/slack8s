422498e0bac7ffca718127f149f722971229ba92
Stephen Favron <stephen@kwlabs.ca>
2016-08-04 16:51:33 -0400

    Refactor some code in main.go and fix an equality inversion

--- main.go
+++ main.go
@@ -26,6 +26,36 @@ type messager interface {
 	sendMessage(message) error
 }
 
+func getWhitelist(wlStr string) (wl whitelist) {
+	if wlStr == "" {
+		return nil
+	}
+
+	if err := json.Unmarshal([]byte(wlStr), &wl); err != nil {
+		return nil
+	}
+
+	return wl
+}
+
+func getTypes(typeStr string) (tm map[watch.EventType]bool) {
+	if typeStr == "" {
+		return nil
+	}
+
+	var types []watch.EventType
+	if err := json.Unmarshal([]byte(typeStr), &types); err != nil {
+		return nil
+	}
+
+	tm = make(map[watch.EventType]bool)
+	for _, t := range types {
+		tm[t] = true
+	}
+
+	return tm
+}
+
 func main() {
 	cl, err := k8s.NewInCluster()
 	if err != nil {
@@ -34,30 +64,14 @@ func main() {
 
 	kube := &kubeCfg{
 		kubeClient: cl,
+		types:      getTypes(os.Getenv("EVENT_TYPES")),
+		whitelist:  getWhitelist(os.Getenv("WHITELIST")),
 	}
 
 	msgr := &slackCfg{
 		messagePoster: slack.New(os.Getenv("SLACK_TOKEN")),
 		channel:       os.Getenv("SLACK_CHANNEL"),
 		env:           os.Getenv("ENV"),
-	}
-
-	var types []watch.EventType
-	typeString := os.Getenv("EVENT_TYPES")
-	if err = json.Unmarshal([]byte(typeString), &types); typeString != "" && err != nil {
-		typeMap := make(map[watch.EventType]bool)
-
-		for _, t := range types {
-			typeMap[t] = true
-		}
-
-		kube.types = typeMap
-	}
-
-	var wl whitelist
-	whitelistString := os.Getenv("WHITELIST")
-	if err = json.Unmarshal([]byte(whitelistString), &wl); whitelistString != "" && err != nil {
-		kube.whitelist = wl
 	}
 
 	err = kube.watchEvents(msgr)
